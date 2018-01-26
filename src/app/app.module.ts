5564a6219ad955fcfe01ab181f6b01e4c5a9b03e
Yinghao Wang <yinghao.a.wang@gmail.com>
2018-01-25 17:20:55 -0800

    comics module created and working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import { ComicModule } from './comic/comic.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-
-import { ComicsComponent } from './comics/comics.component';
-import { ComicReaderComponent } from './comic-reader/comic-reader.component';
-import { ComicService } from './comic.service';
 
 @NgModule({
     declarations: [
         AppComponent,
-        ComicsComponent,
-        ComicReaderComponent
     ],
     imports: [
+        ComicModule,
         BrowserModule,
-        HttpClientModule,
         AppRoutingModule,
         ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
     ],
-    providers: [ ComicService ],
     bootstrap: [ AppComponent ]
 })
 export class AppModule { }
