4bade0b1f8d5d78fe50df34d093b12ec946564d6
mbadh <mbadh@ucsc.edu>
2018-02-20 18:21:59 -0800

    Created SearchService
    Hooked search bar UI up to search service.
    
    Hooked comics list UI up to search service.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { AuthenticationService } from './user/authentication.service';
 import { ComicService } from './comic/comic.service';
 import { MessageService } from './message/message.service';
+import { SearchService } from './comic/search.service';
 
 @Component({
     selector: 'wcm-root',
@@ -9,14 +10,21 @@ import { MessageService } from './message/message.service';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-
+    query: string;
+    
     constructor(
         public auth: AuthenticationService,
         public comics: ComicService,
-        public messages: MessageService) { }
+        public messages: MessageService,
+        private search: SearchService
+    ) { }
 
     public logout() {
         this.auth.logout();
+    }
+
+    searchComic() {
+        this.search.findComics(this.query);
     }
 
     ngOnInit(): void {
