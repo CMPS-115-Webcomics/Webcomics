91993c6d430857a6c71b3a2ddc36d56e87329b11
James Huang <jameshuanguc@gmail.com>
2018-03-08 22:17:54 -0800

    removed comment

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -2,21 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { ComicService } from '../../comic/comic.service';
 import { Comic } from '../../comic/comic';
 
-
 @Component({
-  selector: 'wcm-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+    selector: 'wcm-profile',
+    templateUrl: './profile.component.html',
+    styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
     public comics: Comic[] = [];
 
-  constructor(private comicService: ComicService) {}
+    constructor(private comicService: ComicService) {}
 
-  ngOnInit() {
-    this.comics = this.comicService.comics;
+    ngOnInit() {
+        this.comics = this.comicService.comics;
         if (this.comicService.comics.length === 0)
-        this.comicService.loadComics();
-  }
-
+            this.comicService.loadComics();
+    }
 }
